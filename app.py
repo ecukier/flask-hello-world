9a5ba7cb5f41baec4b971b5ce6c78fd6943d3c1a
ecukier <87502028+ecukier@users.noreply.github.com>
2025-07-29 22:50:53 -0600

    db_create

--- app.py
+++ app.py
@@ -10,8 +10,26 @@ def hello_world():
     return 'Hello World from Ephram Cukier in 3308'
 
 
-@app.route('/db-test')
+@app.route('/db_test')
 def db_test():
     conn = psycopg.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
     conn.close()
     return 'Database connection succesfull'
+
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
+    cur = conn.cursor()
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.close()
+    return 'Basketball table succesfully created'
