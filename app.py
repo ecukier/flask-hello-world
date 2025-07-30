af36d87ee668029ffac3d67b73294fb12fe265e6
ecukier <87502028+ecukier@users.noreply.github.com>
2025-07-29 22:55:59 -0600

    routes

--- app.py
+++ app.py
@@ -31,5 +31,59 @@ def db_create():
             Number int
             );
     ''')
+    conn.commit()
     conn.close()
     return 'Basketball table succesfully created'
+
+
+@app.route('/db_insert')
+def db_create():
+    conn = psycopg.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
+    cur = conn.cursor()
+
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table succesfully populated'
+
+
+@app.route('/db_select')
+def db_select():
+    conn = psycopg.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+
+    records = cur.fetchall()
+    conn.close()
+
+    response_string = '<table>'
+    for player in records:
+        response_string += '<tr>'
+        for info in player:
+            response_string += f'<td>{info}</td>'
+        response_string += '</tr>'
+    response_string += '</table>'
+    return response_string
+
+
+
+@app.route('/db_drop')
+def db_drop():
+    conn = psycopg.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
+    cur = conn.cursor()
+
+    cur.execute('''
+        DROP TABLE Basketball;
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table succesfully dropped'
