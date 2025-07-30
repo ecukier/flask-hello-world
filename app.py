bc9233718134c9f69a71774c5565748944296ae4
ecukier <87502028+ecukier@users.noreply.github.com>
2025-07-29 22:37:18 -0600

    db test

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Ephram Cukier in 3308'
+
+
+@app.route('/db-test')
+def db_test():
+    conn = psycopg2.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
+    conn.close()
+    return 'Database connection succesfull'
