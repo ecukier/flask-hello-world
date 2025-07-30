ede29755455b8c0a8d727aadfab40d0b8ddfdbc9
ecukier <87502028+ecukier@users.noreply.github.com>
2025-07-29 22:47:12 -0600

    wip

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import psycopg2
+import psycopg
 
 app = Flask(__name__)
 
@@ -12,6 +12,6 @@ def hello_world():
 
 @app.route('/db-test')
 def db_test():
-    conn = psycopg2.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
+    conn = psycopg.connect("postgresql://flask_hello_world_db_3tcd_user:plLtvyBCNtZlopRpBONqG2naXcFO0nQv@dpg-d24q21ali9vc73eigphg-a/flask_hello_world_db_3tcd")
     conn.close()
     return 'Database connection succesfull'
